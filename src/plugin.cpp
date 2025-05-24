838cb733abacf7d6308df5b0293629971fbbd009
Manuel Schneider <manuelschneid3r@gmail.com>
2025-05-24 21:10:10 +0200

    Avoid runtime conversion from utf8 for QString construction

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -6,16 +6,17 @@
 #include <QUrl>
 #include <albert/albert.h>
 #include <albert/standarditem.h>
+using namespace Qt::StringLiterals;
+using namespace albert::util;
 using namespace albert;
 using namespace std;
-using namespace util;
 
 Plugin::Plugin()
 {
-    QFile file(":tlds");
+    QFile file(u":tlds"_s);
     if (!file.open(QIODevice::ReadOnly))
         throw runtime_error("Unable to read tld resource");
-    valid_tlds << QTextStream(&file).readAll().split("\n");
+    valid_tlds << QTextStream(&file).readAll().split(u'\n');
     std::sort(valid_tlds.begin(), valid_tlds.end());
 }
 
@@ -29,27 +30,28 @@ vector<RankItem> Plugin::handleGlobalQuery(const Query &query)
     if (!url.isValid())
         return results;
 
-    if ((url.scheme() == "http" || url.scheme() == "https")){
+    if ((url.scheme() == u"http"_s || url.scheme() == u"https"_s)){
 
-        auto tld = url.host().section('.', -1, -1);
+        auto tld = url.host().section(u'.', -1, -1);
 
         // Skip tld only queries
         if (trimmed.size() == tld.size())
             return results;
 
         // validate top level domain if scheme is not given (http assumed)
-        if (tld.size() == 0 || (!trimmed.startsWith("http") && !binary_search(valid_tlds.begin(), valid_tlds.end(), tld)))
+        if (tld.size() == 0 || (!trimmed.startsWith(u"http"_s)
+                                && !binary_search(valid_tlds.begin(), valid_tlds.end(), tld)))
             return results;
 
         results.emplace_back(
             StandardItem::make(
-                "url_hanlder",
+                u"url_hanlder"_s,
                 tr("Open URL in browser"),
                 tr("Open %1").arg(url.authority()),
-                {"xdg:www", "xdg:web-browser", "xdg:emblem-web", ":default"},
+                {u"xdg:www"_s, u"xdg:web-browser"_s, u"xdg:emblem-web"_s, u":default"_s},
                 {
                     {
-                        "open_url", tr("Open URL"),
+                        u"open_url"_s, tr("Open URL"),
                         [url](){ open(url); }
                     }
                 }
