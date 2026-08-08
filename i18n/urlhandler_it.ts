713de21d0a8d9234420b9e853ea75b3694a221f6
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/urlhandler_it.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Open URL in browser</source>
+        <translation>Apri URL nel browser</translation>
+    </message>
+    <message>
+        <source>Open %1</source>
+        <translation>Apri %1</translation>
+    </message>
+    <message>
+        <source>Open URL</source>
+        <translation>Apri URL</translation>
+    </message>
+</context>
+</TS>
